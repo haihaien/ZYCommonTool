1e647390dbd717ea5dd3552315bf5e6f9012c85c
LeMo-test <lemo-test@lemo-testdeimac.local>
2016-06-13 15:15:10 +0800

    Initial Commit

---
+++ ZYControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYControl
+//
+//  Created by LeMo-test on 16/6/13.
+//  Copyright © 2016年 LeMo-test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYControl
+//
+//  Created by LeMo-test on 16/6/13.
+//  Copyright © 2016年 LeMo-test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
